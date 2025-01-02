a890ca7921db3c32faefb291a4ed87c44945b859
shawn buckhannon <shawnbuckhannon@yahoo.com>
2025-01-02 08:26:04 -0500

    Fix import of User model in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = /models/User("./User");
+const User = require("./User");
 
 const router = express.Router();
 
